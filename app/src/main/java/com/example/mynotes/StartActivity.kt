bcb251e4a9b6f259ec2d20b32b6d1e3ba68dcc58
Kavindujayasekara <kavindujayasekara3058@gmail.com>
2024-05-13 01:39:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mynotes/StartActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mynotes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+        // Create an intent to start MainActivity
+        val intentPage = findViewById<Button>(R.id.go_btn)
+        intentPage.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
